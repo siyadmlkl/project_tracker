368473ee2e4b68f93b3ca935409c8f0c3a6e02ad
masiyad <31222993+siyadmlkl@users.noreply.github.com>
2023-01-18 10:15:40 +0000

    Modified

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def addProject():
     return render_template('addproject.html',**context)
 
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
